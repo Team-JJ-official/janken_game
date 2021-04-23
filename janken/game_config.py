a41f0ab881ceffa1d0e0c30b6c91da6fc5a1a5e6
jj_ <avasoldier2@gmail.com>
2021-04-23 17:08:36 +0900

    game_config.py

--- janken/game_config.py
+++ janken/game_config.py
@@ -1,4 +1,7 @@
+from typing import List, Union
 import json
+import os
+import glob
 
 import pygame
 
@@ -16,14 +19,47 @@ class GameConfig:
         self.load()
     
     def load(self):
+        """全てを読み込む
+        """
         self.load_stages()
-        self.load_images()
+        self.load_components()
+    
+    def load_json(self, path: str) -> dict:
+        """JSONを読み込んで辞書を返す．
+        """
+        with open(path, "r") as f:
+            dic = json.load(f)
+        return dic
+    
+    def _load_surface(self, path: str) -> pygame.surface.Surface:
+        """ファイル名を指定すると，画像をSurfaceで読み込んで返す．
+        """
+        return pygame.image.load(path)
+    
+    def _load_surfaces(self, dir_path: str) -> List[pygame.surface.Surface]:
+        """ディレクトリを指定すると，内部のファイルを全て読み込んで\
+        Surfaceのリストで返す．リストはファイル名でソートされている．
+        """
+        surfaces = []
+        for path in sorted(glob.glob(os.path.join(dir_path, "*"))):
+            surfaces.append(self._load_surface(path))
+        return surfaces
+    
+    def _load_surface_from_path_or_dir(self, path: str) -> Union[pygame.surface.Surface, List[pygame.surface.Surface]]:
+        """ファイル名を指定した場合はSurfaceを，\
+        ディレクトリを指定した場合はSurfaceのリストを返す．
+        """
+        if os.path.isdir(path):
+            return self._load_surfaces(dir_path=path)
+        else:
+            return self._load_surface(path=path)
     
     def load_stages(self):
-        tmp_dic = self.dic.get("stage")
+        """self.stagesにステージを読み込む．
+        """
+        tmp_dic = self.dic.get("stages")
         if tmp_dic:
-            with open(tmp_dic["path"], "r") as f:
-                json_data = json.load(f)
+            json_data = self.load_json(tmp_dic["path"])
             
             self.stages = {key: Stage(key, dic["name"], pygame.image.load(dic["path"])) for key, dic in sorted(json_data.items(), key=lambda x: -int(x[0]), reverse=True)}
             print(self.stages)
@@ -34,9 +70,19 @@ class GameConfig:
     def load_players(self):
         pass
     
-    def load_images(self):
-        import glob
+    def load_components(self):
+        """self.componentsに，共通して使いそうな画像類を読み込む．
+        """
+        tmp_dic = self.dic.get("components")
+        if tmp_dic:
+            json_data = self.load_json(tmp_dic["path"])
+
+            self.components = {
+                name: self._load_surface_from_path_or_dir(dic["path"])
+                for name, dic in json_data.items()
+            }
 
 
 if __name__ == "__main__":
     game_config = GameConfig("./jsons/config.json")
+    print()
