9866449f9f0b13b7c58d51a256d28b67bf0ad09e
jj_ <avasoldier2@gmail.com>
2021-04-24 11:59:00 +0900

    loading.py GameConfigの読み込み

--- janken/loading.py
+++ janken/loading.py
@@ -2,6 +2,7 @@ from typing import Dict, List, Callable, Tuple
 from enum import Enum
 import math
 import random
+import time
 from threading import Thread
 
 import pygame
@@ -11,12 +12,15 @@ from pygame.sprite import Sprite
 from stage import Stage
 from screen import Screen, BaseScreen
 from sprites import SimpleSprite, TextSprite, AlignSprite, load_animation_sprite
+from game_config import GameConfig
 
-class Loading(BaseScreen):
+class LoadingScreen(BaseScreen):
     def __init__(self):
         super().__init__()
         self.result = Screen.START
         self.fps = 60
+
+        self.game_config = None
 
         bg_image = pygame.image.load("./images/components/bg.jpeg").convert_alpha()
         bg_sprite = SimpleSprite(bg_image.get_rect(), bg_image)
@@ -29,9 +33,9 @@ class Loading(BaseScreen):
     def init(self):
         """全てのローディング処理
         """
-        import time
+        time.sleep(2)
         pygame.font.init()
-        time.sleep(4)
+        self.game_config = GameConfig("./jsons/config.json")
         self.run = False
         self.next_screen = Screen.START
     
@@ -47,7 +51,18 @@ class Loading(BaseScreen):
             self.clock.tick(60)
 
 if __name__ == "__main__":
+    from stage_select import StageSelectScreen
+    from title import TitleScreen
+
     pygame.init()
     pygame.display.set_mode((500, 500))
-    loading = Loading()
+    loading = LoadingScreen()
     loading.main()
+
+    game_config = loading.game_config
+
+    start_screen = TitleScreen(game_config)
+    start_screen.main()
+
+    stage_select_screen = StageSelectScreen(game_config, None)
+    stage_select_screen.main()
